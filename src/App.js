95788bd00dae0f56c8b9990976e5fa426e306e63
Ahmed Abdullah <ahmed067abdullah@gmail.com>
2021-01-12 11:46:45 +0500

    simple editor implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Editor } from 'react-draft-wysiwyg';
+import {
+  convertToRaw,
+  EditorState,
+  convertFromRaw,
+} from 'draft-js';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+// import draftToHtml from 'draftjs-to-html';
 
-function App() {
+const he = require('he')
+
+const App = () => {
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+  useEffect(() => {
+    const content = localStorage.getItem("blog");
+    if (content) {
+      try {
+        const editorState = EditorState.createWithContent(convertFromRaw(JSON.parse(he.decode(content))))
+        setEditorState(editorState)
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <button
+        className="btn"
+        style={{ 'right': 80 }}
+        onClick={() => {
+          const conent = JSON.stringify(convertToRaw(editorState.getCurrentContent()));
+          localStorage.setItem("blog", conent);
+        }}>Save
+      </button>
+      <button
+        className="btn"
+        style={{ 'right': 20 }}
+        onClick={() => {
+          localStorage.removeItem("blog");
+          setEditorState(EditorState.createEmpty());
+        }}>Clear
+      </button>
+      <Editor
+        editorState={editorState}
+        toolbarClassName="toolbarClassName"
+        wrapperClassName="wrapperClassName"
+        editorClassName="editorClassName"
+        onEditorStateChange={setEditorState}
+        toolbarStyle={{
+          'position': 'fixed',
+          'top': 0,
+          'left': 0,
+          'zIndex': '2'
+        }}
+        editorStyle={{
+          marginTop: 50
+        }}
+      />
+      {/* To render the created blog */}
+      {/* <div dangerouslySetInnerHTML={{__html: draftToHtml(convertToRaw(editorState.getCurrentContent()))}}>
+    </div> */}
+    </div >
   );
 }
 
